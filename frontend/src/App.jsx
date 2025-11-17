dc905bb930aec442e4bc98de6631ffe8e3b1d19b
Nilanjan Saha <nilanjan.s.1920@gmail.com>
2025-11-17 17:26:38 +0530

    initial changes - frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import MenuBar from "./components/MenuBar";
+import LandingPage from "./pages/LandingPage";
+import Dashboard from "./pages/Dashboard";
+import MainPage from "./pages/MainPage";
+import PreviewPage from "./pages/PreviewPage";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <MenuBar />
+      <Toaster />
+
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/generate" element={<MainPage />} />
+        <Route path="/preview" element={<PreviewPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
